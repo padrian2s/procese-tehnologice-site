9c435334effece96402b1343fe4a93d4e20af464
adrian <adrian.paleacu@gmail.com>
2025-12-26 12:43:42 +0200

    Fix GitHub Pages basePath

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const isGitHubPages = process.env.NEXT_OUTPUT === 'export';
 
 const nextConfig: NextConfig = {
   output: isGitHubPages ? 'export' : 'standalone',
+  basePath: isGitHubPages ? '/procese-tehnologice-site' : '',
+  assetPrefix: isGitHubPages ? '/procese-tehnologice-site/' : '',
   images: {
     unoptimized: true,
     remotePatterns: [
